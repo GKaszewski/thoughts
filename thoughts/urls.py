a5ce05dae447642dcb0e19d11017ac84a0c11f91
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2024-05-10 19:34:47 +0200

    Serve static files in debug mode, add blog styling

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
